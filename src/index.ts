791d2458e07e15f7bbf80f3b4fc302c492927154
Sharma <PRSHA@barco.com>
2017-05-25 10:56:40 +0530

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SampleComponent } from './sample.component';
+
+export * from './sample.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    SampleComponent
+  ],
+  exports: [
+    SampleComponent
+  ]
+})
+export class SampleModule {}
---
+++ src/sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-cmp',
+  template: `<h1>Sample component</h1>`
+})
+export class SampleComponent {
+
+  constructor() {
+  }
+
+}
